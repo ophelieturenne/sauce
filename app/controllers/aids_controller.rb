558aa7dcc66ffe4a529d0cb605dd09de17828574
ophelie turenne <turenneophelie@gmail.com>
2022-10-19 20:43:04 +0400

    completed pundit for emergency and aid

--- app/controllers/aids_controller.rb
+++ app/controllers/aids_controller.rb
@@ -2,17 +2,16 @@ class AidsController < ApplicationController
   skip_before_action :authenticate_user!, only: [:index]
 
   def index
-    @aids = Aid.all
-
     if params[:query].present?
-      @aids = Aid.where("name ILIKE ?", "%#{params[:query]}%")
+      @aids = policy_scope(Aid.where("name ILIKE ?", "%#{params[:query]}%"))
     else
-      @aids = Aid.all
+      @aids = policy_scope(Aid)
     end
   end
 
   def show
     @aid = Aid.find(params[:id])
     @steps = Step.where(aid: @aid)
+    authorize @aid
   end
 end
