233bf4c1e34ca7aaf6661feaeaf20282d1ba28d3
ophelie turenne <turenneophelie@gmail.com>
2022-10-19 14:38:49 +0400

    added pundit with emergency controller and view

--- app/controllers/emergencies_controller.rb
+++ app/controllers/emergencies_controller.rb
@@ -1,19 +1,22 @@
 class EmergenciesController < ApplicationController
   def index
-    @emergencies = Emergency.all
+    @emergencies = policy_scope(Emergency)
   end
 
   def show
     @emergency = Emergency.find(params[:id])
+    authorize @emergency
   end
 
   def new
     @emergency = Emergency.new
+    authorize @emergency
   end
 
   def create
     @emergency = Emergency.new(emergency_params)
     @emergency.user = current_user
+    authorize @emergency
     if @emergency.save
       redirect_to emergencies_path
     else
@@ -24,10 +27,12 @@ class EmergenciesController < ApplicationController
 
   def edit
     @emergency = Emergency.find(params[:id])
+    authorize @emergency
   end
 
   def update
     @emergency = Emergency.find(params[:id])
+    authorize @emergency
     if @emergency.update(emergency_params)
       redirect_to emergencies_path, notice: "list was successfully updated."
     else
@@ -37,6 +42,7 @@ class EmergenciesController < ApplicationController
 
   def destroy
     @emergency = Emergency.find(params[:id])
+    authorize @emergency
     @emergency.destroy
     redirect_to emergencies_path, status: :see_other
   end
