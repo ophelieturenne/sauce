1d32d5e0f0198905fff084dee450c283b75f266f
ophelie turenne <turenneophelie@gmail.com>
2022-10-21 00:27:06 +0400

    added location and coordinates

--- app/javascript/controllers/map_controller.js
+++ app/javascript/controllers/map_controller.js
@@ -1,10 +1,12 @@
 import { Controller } from "@hotwired/stimulus"
 import mapboxgl from "mapbox-gl"
+// import MapboxGeocoder from "@mapbox/mapbox-gl-geocoder"
+
+
 // Connects to data-controller="map"
 export default class extends Controller {
   static values = {
     apiKey: String,
-
   }
 
   connect() {
@@ -13,11 +15,38 @@ export default class extends Controller {
     this.map = new mapboxgl.Map({
       container: this.element, // container ID
       style: 'mapbox://styles/mapbox/streets-v10', // style URL
-      center: [-74.5, 40], // starting position [lng, lat]
+      center: [57.5522, -20.3484], // starting position [lng, lat]
       zoom: 9, // starting zoom
       });
-      map.on('style.load', () => {
-      // map.setFog({}); // Set the default atmosphere style
+
+      // this.ll = new mapboxgl.LngLat(-123.9749, 40.7736);
+      // console.log(ll.lng);
+
+    // Add geolocate control to the map.
+    this.map.addControl(
+      new mapboxgl.GeolocateControl({
+      positionOptions: {
+      enableHighAccuracy: true
+      },
+      // When active the map will receive updates to the device's location as it changes.
+      trackUserLocation: true,
+      // Draw an arrow next to the location dot to indicate which direction the device is heading.
+      showUserHeading: true
+      })
+    );
+
+    this.map.on('click', (e) => {
+      document.getElementById('info').innerHTML =
+      // `e.lngLat` is the longitude, latitude geographical position of the event.
+      JSON.stringify(e.lngLat.wrap());
+
     });
+
+
+    ///
+
+
   }
+
+
 }
