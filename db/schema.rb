0c1b8aa4b5f191d970d6d812eb1c11c0a69bb8bd
Romain Thiboudois <romainthiboudois@gmail.com>
2022-10-25 11:00:52 +0400

    modified grid

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_10_24_075619) do
+ActiveRecord::Schema[7.0].define(version: 2022_10_24_080531) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -56,9 +56,28 @@ ActiveRecord::Schema[7.0].define(version: 2022_10_24_075619) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.bigint "user_id", null: false
-    t.boolean "ispublic", default: false
     t.string "phone_number"
+    t.boolean "ispublic", default: false
     t.index ["user_id"], name: "index_emergencies_on_user_id"
+  end
+
+  create_table "questions", force: :cascade do |t|
+    t.string "option1"
+    t.string "option2"
+    t.string "option3"
+    t.string "option4"
+    t.string "answer"
+    t.string "question"
+    t.bigint "quiz_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["quiz_id"], name: "index_questions_on_quiz_id"
+  end
+
+  create_table "quizzes", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "steps", force: :cascade do |t|
@@ -86,5 +105,6 @@ ActiveRecord::Schema[7.0].define(version: 2022_10_24_075619) do
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
   add_foreign_key "active_storage_variant_records", "active_storage_blobs", column: "blob_id"
   add_foreign_key "emergencies", "users"
+  add_foreign_key "questions", "quizzes"
   add_foreign_key "steps", "aids"
 end
