7b4ac1c854b2e6896bba81cf0a3ef79b071d95c0
Frederic Feliciane <fredericfeliciane8@gmail.com>
2022-10-25 12:24:57 +0400

    home page - background image added to home page

--- app/javascript/controllers/map_controller.js
+++ app/javascript/controllers/map_controller.js
@@ -1,17 +1,15 @@
 import { Controller } from "@hotwired/stimulus"
 import mapboxgl from "mapbox-gl"
 // import MapboxGeocoder from "@mapbox/mapbox-gl-geocoder"
-
-
-// Connects to data-controller="map"
+​
 export default class extends Controller {
   static values = {
     apiKey: String,
   }
-
+​
   connect() {
     mapboxgl.accessToken = this.apiKeyValue
-
+​
     this.map = new mapboxgl.Map({
       container: this.element, // container ID
       style: 'mapbox://styles/mapbox/streets-v10', // style URL
@@ -19,11 +17,7 @@ export default class extends Controller {
       zoom: 9,
       // starting zoom
     });
-
-      // this.ll = new mapboxgl.LngLat(-123.9749, 40.7736);
-      // console.log(ll.lng);
-
-    // Add geolocate control to the map.
+​
     this.map.addControl(
       new mapboxgl.GeolocateControl({
       positionOptions: {
@@ -35,27 +29,52 @@ export default class extends Controller {
       showUserHeading: true
       })
     );
-
+​
     this.map.on('click', (e) => {
-      document.getElementById('info').innerHTML =
       // `e.lngLat` is the longitude, latitude geographical position of the event.
-      JSON.stringify(e.lngLat.wrap());
-
+      document.getElementById('info').innerHTML = JSON.stringify(e.lngLat.wrap());
     });
-
-    setTimeout(function() {  myLocation(); }, 1500);
-    ///
+​
+    setTimeout(function() {  myLocation(); }, 3000);
+​
+    navigator.geolocation.getCurrentPosition(position => {
+      // if (this.map.loaded()) {
+        const lng = position.coords.longitude;
+        const lat = position.coords.latitude;
+        console.log('Your current position is:');
+        console.log(`Latitude : ${lat}`);
+        console.log(`Longitude: ${lng}`);
+        const url = `https://api.mapbox.com/geocoding/v5/mapbox.places/${lng}%2C%20${lat}.json?access_token=${this.apiKeyValue}`
+        fetch(url)
+          .then(response => response.json())
+          .then((data) => {
+            const address = data.features[0].place_name;
+            // console.log(address);
+            const popup = new mapboxgl.Popup().setHTML(`<h2>${address}</h2>`)
+            // Create a HTML element for your custom marker
+            const customMarker = document.createElement("div")
+            customMarker.className = "marker"
+            // customMarker.style.backgroundImage = `url('https://d26jy9fbi4q9wx.cloudfront.net/assets/logo-ae2beeecce25d711f577b08deb9adfc6c02b673ed106b8d6c3da0f1721d9da33.svg')`
+            // customMarker.style.backgroundSize = "contain"
+            customMarker.style.width = "25px"
+            customMarker.style.height = "25px"
+​
+            // Pass the element as an argument to the new marker
+            new mapboxgl.Marker(customMarker)
+              .setLngLat([lng, lat])
+              .setPopup(popup)
+              .addTo(this.map)
+            this.popItUp();
+          })
+      // }
+    })
   }
-
-  #fetchAddress() {
-    url = `https://api.mapbox.com/geocoding/v5/mapbox.places/${lat}%2C%20${long}.json?access_token=${key}`
-
+​
+  popItUp() {
+    document.querySelector(".marker.mapboxgl-marker.mapboxgl-marker-anchor-center").click();
   }
-
 }
-
-
+​
 function myLocation() {
-  // console.log("test")
   document.querySelector(".mapboxgl-ctrl-icon").click();
 }
