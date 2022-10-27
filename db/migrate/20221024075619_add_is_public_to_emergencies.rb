d838c1fed4f6086838f589f23c03a50075142b2b
Ophelie M S Turenne <44888967+ophelieturenne@users.noreply.github.com>
2022-10-27 17:31:06 +0400

    Update 20221024075619_add_is_public_to_emergencies.rb

--- db/migrate/20221024075619_add_is_public_to_emergencies.rb
+++ db/migrate/20221024075619_add_is_public_to_emergencies.rb
@@ -1,5 +1,5 @@
 class AddIsPublicToEmergencies < ActiveRecord::Migration[7.0]
   def change
-    add_column :emergencies, :ispublic, :boolean, default: false
+    add_column :emergencies, :ispublic, :boolean, default: :false
   end
 end
